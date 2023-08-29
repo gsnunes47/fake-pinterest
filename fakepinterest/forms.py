52397651aadfe373d733b2b9c37a43325a2f4210
Gustavo Nunes <gustavompn06@gmail.com>
2023-08-29 08:12:31 -0300

    Criação dos formulários e validação dos mesmos.

---
+++ fakepinterest/forms.py
@@ -0,0 +1,22 @@
+#Criar os formularios do site
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
+from fakepinterest.models import Usuario
+
+class FormLogin(FlaskForm):
+    email = StringField("E-mail", validators=[DataRequired(), Email()]) 
+    senha = PasswordField("Senha", validators=[DataRequired()])
+    botao_confirmacao("Fazer Login")
+    
+class FormCriarConta(FlaskForm):
+    email = StringField("E-mail", validators=[DataRequired(), Email()])
+    username = StringField("Username", validators=[DataRequired()])
+    senha = PasswordField("Senha", validators=[DataRequired(), Length(6, 20)])
+    confirmacao_senha = PasswordField("Confirme a Senha", validators=[DataRequired(), EqualTo("senha")])
+    botao_confirmacao = SubmitField("Criar Conta")
+
+    def validate_email(self, email):
+        usuario = Usuario.query.filter_by(email=email).first()
+        if usuario:
+            return ValidationError("E-mail já cadastrado")
