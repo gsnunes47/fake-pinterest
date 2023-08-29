839e494f85ae18d9c195f21fc3cd44e48049acd2
Gustavo Nunes <gustavompn06@gmail.com>
2023-08-29 07:57:46 -0300

    Criação da função de carregar um usuario, implemetação da chave de segurança e atualização da rota do perfil

--- fakepinterest/models.py
+++ fakepinterest/models.py
@@ -1,6 +1,11 @@
 #Criar a estrutura do banco de dados
-from fakepinterest import database
+from fakepinterest import database, login_manager
 from datetime import datetime
+from flask_login import UserMixin
+
+@login_manager.user_loader
+def load_usuario(id_usuario):
+    return Usuario.query.get(int(id_usuario))
 
 class Usuario(database.Model):
     id = database.Column(database.Integer, primary_key=True)
