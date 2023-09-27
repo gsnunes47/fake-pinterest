5eee88c074b441db86005bba6aee8113667cbdca
Gustavo Nunes <gustavompn06@gmail.com>
2023-09-27 09:25:36 -0300

    Corrigindo validações de autenticação.

--- fakepinterest/forms.py
+++ fakepinterest/forms.py
@@ -8,7 +8,12 @@ class FormLogin(FlaskForm):
     email = StringField("E-mail", validators=[DataRequired(), Email()]) 
     senha = PasswordField("Senha", validators=[DataRequired()])
     botao_confirmacao = SubmitField("Fazer Login")
-    
+
+    def validate_email(self, email):
+        usuario = Usuario.query.filter_by(email=email.data).first()
+        if not usuario:
+            raise ValidationError("Usuário inexistente, crie uma conta.")
+
 class FormCriarConta(FlaskForm):
     email = StringField("E-mail", validators=[DataRequired(), Email()])
     username = StringField("Username", validators=[DataRequired()])
@@ -19,8 +24,9 @@ class FormCriarConta(FlaskForm):
     def validate_email(self, email):
         usuario = Usuario.query.filter_by(email=email.data).first()
         if usuario:
-            return ValidationError("E-mail já cadastrado")
+            raise ValidationError("E-mail já cadastrado, faça login para continuar.")
 
 class FormFoto(FlaskForm):
     foto = FileField("Foto", validators=[DataRequired()])
     botao_confirmacao = SubmitField("Enviar Foto")
+    
