af7ad24c84d2578a57c374cdcfd85169ff7cd480
Gustavo Nunes <gustavompn06@gmail.com>
2023-09-27 10:39:20 -0300

    Atualização de requirements.txt e alteração para variáveis de ambiente

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -2,10 +2,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+import os
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
 app.config["SECRET_KEY"] = "c401f29fac31b7c2972f29d97a4f37c6"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
 
 database = SQLAlchemy(app)
